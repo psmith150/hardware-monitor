fbcbcf4d05058de07aff532b3749e17d2c3ac65a
psmith150 <patricksmith150@gmail.com>
2020-05-18 21:05:23 -0500

    Configured rotating file logging.

--- hardware_monitor.py
+++ hardware_monitor.py
@@ -1,16 +1,24 @@
-import psutil, paho.mqtt.client as mqtt, time, logging
+import psutil, paho.mqtt.client as mqtt, time, logging, logging.handlers, os
 
-HARDWARE_POLL_INTERVAL = 10
-MQTT_TOPIC_PREFIX = 'app_server/hardware/'
-MQTT_HOST = '192.168.1.10'
-MQTT_KEEP_ALIVE = 60
+HARDWARE_POLL_INTERVAL = 10 # Seconds between polling of hardware
+MQTT_TOPIC_PREFIX = 'app_server/hardware/' # MQTT topic to use as prefix
+MQTT_HOST = '192.168.1.10' # MQTT host
+MQTT_KEEP_ALIVE = 60 # Keep alive time
+LOG_PATH = 'logs\\'
 
 def on_connect(client, userdata, flags, rc):
     client.publish(MQTT_TOPIC_PREFIX + "available", "online")
 
 def main():    
     # Set up logging
-    logging.basicConfig(filename='hardware_monitor.log', level=logging.WARNING, format='%(asctime)s - %(levelname)s:%(message)s')
+    # Create path if doesn't exist
+    os.makedirs(LOG_PATH, exist_ok=True)
+    fileLogger = logging.getLogger('FileLogger')
+    fileLogger.setLevel(logging.WARNING)
+    handler = logging.handlers.RotatingFileHandler(filename=LOG_PATH + 'hardware_monitor.log', maxBytes=1024, backupCount=5)
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s:%(message)s')
+    handler.setFormatter(formatter)
+    fileLogger.addHandler(handler)
 
     # Set up MQTT
     client = mqtt.Client("app_server_system_monitor")
@@ -26,7 +34,7 @@ def main():
 
     done = False
     hardwareStartTime = hardwareEndTime = time.time()
-    logging.info('Starting system monitoring')
+    fileLogger.info('Starting system monitoring')
     while not done:
         hardwareEndTime = time.time()
         # Hardware polling
@@ -35,15 +43,14 @@ def main():
                 cpuPercent = psutil.cpu_percent()
                 memoryUtilization = psutil.virtual_memory().percent
                 diskPercent = psutil.disk_usage('/').percent
-                #print(f'CPU: {cpuPercent}\nMemory: {memoryUtilization}\nDisk: {diskPercent}\n')
-                logging.info(f'CPU: {cpuPercent}\nMemory: {memoryUtilization}\nDisk: {diskPercent}\n')
+                fileLogger.info(f'CPU: {cpuPercent}% - Memory: {memoryUtilization}% - Disk: {diskPercent}%\n')
                 client.publish(MQTT_TOPIC_PREFIX + "cpu", cpuPercent)
                 client.publish(MQTT_TOPIC_PREFIX + "memory", memoryUtilization)
                 client.publish(MQTT_TOPIC_PREFIX + "disk", diskPercent)
                 client.publish(MQTT_TOPIC_PREFIX + "available", "online")
                 hardwareStartTime = time.time()
             except Exception as ex:
-                logging.error(f'Exception occurred: {ex}')
+                fileLogger.exception(f'Exception occurred: {ex}')
                 raise
         time.sleep(1)
     client.publish(MQTT_TOPIC_PREFIX + "available", "offline").wait_for_publish()
